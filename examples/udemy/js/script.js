9aae5575e8e35deb6964cb1c517db1f93abe0023
dmitry <dmipushkarjov@yandex.ru>
2021-01-06 21:46:17 +0300

    Практика 3

--- examples/udemy/js/script.js
+++ examples/udemy/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt('How many films did you see?', '');
+let numberOfFilms;
 
 const personalMovieDB = {
     
@@ -12,31 +12,62 @@ const personalMovieDB = {
 
 };
 
+start();
+rememberMyFilms();
+detectPersonalLevel();
+showMyDB(personalMovieDB.privat);
+writeYourGenres();
 
-for (let i = 1; i <= numberOfFilms; i++) {
+function start() {
+    numberOfFilms = +prompt('How many films did you see?', '');
 
-    let film = prompt('One of the last watched film', '');
-    let filmScore = prompt('What is the score you estimate this film?', '');
-
-    if (!film || !filmScore) {
-        alert('you need enter something');
-        i--;
-        continue;       
-    } else if (film.length > 50 || filmScore > 50) {
-        i--;
-        continue;
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films did you see?', '');
     }
-
-    personalMovieDB.movies[film] = filmScore;
-
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Too less films watched');
-} else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
-    alert('You are a classical watchman');
-} else {
-    alert('You are wannabe');
+function rememberMyFilms() {
+    for (let i = 1; i <= 2; i++) {
+
+        const film = prompt('One of the last film you watched', ''),
+              filmScore = prompt('What is the score you estimate this film?', '');
+    
+        if (film != '' && filmScore != '' && film.length < 50 && film != null && filmScore != null) {
+            personalMovieDB.movies[film] = filmScore;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+           
+    }
 }
- 
-console.log(personalMovieDB);
+
+function detectPersonalLevel() {
+    
+    if (personalMovieDB.count < 10) {
+        alert('Too less films watched');
+    } else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
+        alert('You are a classical watchman');
+    } else {
+        alert('You are wannabe');
+    }
+     
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } else {
+        return;
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        const yourGenre = prompt(`Your favourite genre under the number ${i + 1}:`, '');
+        personalMovieDB.genres[i] = yourGenre;
+    }
+}
+
+console.log(personalMovieDB); 
