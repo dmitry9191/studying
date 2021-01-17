8f9470fec4d27dd982ca536c6f593d5a24045471
dmitry <dmipushkarjov@yandex.ru>
2021-01-17 20:23:48 +0300

    Доделал project, добавил domNav

---
+++ examples/udemy/domNav/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName === "#text") {
+        continue;
+    } 
+    console.log(node);
+}
--- examples/udemy/project/js/script.js
+++ examples/udemy/project/js/script.js
@@ -14,31 +14,80 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const adversity = document.querySelectorAll('.promo__adv img');
-adversity.forEach(item => item.remove());
-
-document.querySelector('.promo__genre').textContent = 'драма';
-
-const promoBg = document.querySelector('.promo__bg');
-
-promoBg.style.backgroundImage = 'url("img/bg.jpg")';
-
-const filmList = document.querySelectorAll('.promo__interactive-list .promo__interactive-item');
-movieDB.movies.sort();
-
-filmList.forEach((item, i) => {
-    item.textContent = `${i + 1}. ${movieDB.movies[i]}`;
-    i++;
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adversity = document.querySelectorAll('.promo__adv img'),
+          promoBg = document.querySelector('.promo__bg'),
+          checkbox = document.querySelector('.add input[type="checkbox"]'),
+          genre = document.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list');
+    
+    adversity.forEach(item => item.remove());
+    
+    genre.textContent = 'драма';
+    
+    promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+    
+    document.querySelector('.add button').addEventListener('click', addFilm);
+    
+    getSort();
+    
+    function addFilm(e) {
+        
+        e.preventDefault();
+        const input = document.querySelector('.adding__input');
+    
+        if (input.value) {
+            if (input.value.length > 21) {
+                input.value = cutName(input.value);
+            }
+            
+            movieDB.movies.push(input.value);
+            getSort();
+            check(checkbox);
+        }
+    
+    }
+    
+    function getSort() {
+        movieList.innerHTML = '';
+        movieDB.movies.sort();
+        movieDB.movies.forEach((film, i) => {
+            movieList.innerHTML += `
+                <li class="promo__interactive-item">${i + 1}. ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+        console.log(movieDB.movies);
+        document.querySelectorAll('.delete').forEach((item, i) => {
+            item.addEventListener('click', () => {
+                item.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+    
+                getSort();
+            });
+        });
+    
+    }
+    
+    function cutName(inputValue) {
+        const name = inputValue.slice(0, 22);
+        return `${name}...`;
+    }
+    
+    function check(elem) {
+        if (elem.checked) {
+            console.log('Добавляем любимый фильм');
+        }
+    }
 });
-
-console.log(promoBg);
