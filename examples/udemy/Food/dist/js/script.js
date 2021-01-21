5a012c5106a67db210cdd486453f7923fd131ce6
dmitry <dmipushkarjov@yandex.ru>
2021-01-21 21:57:23 +0300

    Написал скрипт для Food

--- examples/udemy/Food/dist/js/script.js
+++ examples/udemy/Food/dist/js/script.js
@@ -1,5 +1,40 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    hideTabContent();
+    showTabContent();
+
+    function hideTabContent() {
+        tabsContent.forEach((item) => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach((item) => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(item = 0) {
+        tabsContent[item].classList.add('show', 'fade');
+        tabsContent[item].classList.remove('hide');
+        tabs[item].classList.add('tabheader__item_active');
+    }
+
+    tabsParent.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, index) => {
+                if (e.target === item) {
+                    hideTabContent();
+                    showTabContent(index);
+                }
+            });
+        }
+    });
+
 });
