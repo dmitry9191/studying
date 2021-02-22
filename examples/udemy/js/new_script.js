437a4b1cdebac22cb66132eca1507bb628500591
dmitry <dmipushkarjov@yandex.ru>
2021-02-22 12:25:39 +0300

    jsAnimation

--- examples/udemy/js/new_script.js
+++ examples/udemy/js/new_script.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log( 'a' > 'B' );
+const now = new Date();
+
+console.log(now.setHours(18));
+console.log(now);
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+console.log(end - start);
