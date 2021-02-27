a7bac41b83cb1c26e0f078590de3c94fa561e955
dmitry <dmipushkarjov@yandex.ru>
2021-02-27 21:15:23 +0300

    added timer

--- examples/udemy/Food/dist/js/script.js
+++ examples/udemy/Food/dist/js/script.js
@@ -2,6 +2,8 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -37,4 +39,58 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Timer
+
+    const deadline = '2021-03-01';
+
+    setTimer(deadline, '.timer');
+
+    function getTimeDifference(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(total / (1000 * 3600 * 24)),
+              hours = Math.floor(total / (1000 * 3600) % 24),
+              minutes = Math.floor((total / 1000 / 60) % 60),
+              seconds =  Math.floor((total / 1000) % 60);
+        
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setTimer(endtime, selector) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeDifference(endtime);
+
+            days.textContent = getZero(t.days);
+            hours.textContent = getZero(t.hours);
+            minutes.textContent = getZero(t.minutes);
+            seconds.textContent = getZero(t.seconds);
+
+            if (t.total < 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+        
 });
