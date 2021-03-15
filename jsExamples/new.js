2126b732e9f387dfebf5ccf0dda4ae9383683560
dmitry <dmipushkarjov@yandex.ru>
2021-03-15 21:34:51 +0300

    promise

--- jsExamples/new.js
+++ jsExamples/new.js
@@ -1,17 +1,35 @@
 'use strict';
 
-class PowerArray extends Array {
-    isEmpty() {
-        return this.length === 0;
-    }
+console.log('Запрос данных...');
 
-    static get [Symbol.species]() {
-        return Array;
-    }
-}
+const req = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        console.log('Подготовка данных...');
+    
+        const product = {
+            name: 'TV',
+            price: 2000
+        };
+    
+        resolve(product);
+    }, 2000);
+});
 
-const arr = new PowerArray(1, 2, 3, 4);
-const filteredArr = arr.filter(item => item >= 10);
-
-console.log(filteredArr);
+req.then((product) => {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            product.status = 'order',
+            resolve(product);
+        }, 2000);
+    });
+}).then((data) => {
+    data.modify = true;
+    return data;
+}).then(data => {
+    console.log(data);
+}).catch(() => {
+    console.error('error');
+}).finally(() => {
+    console.log('finally');
+});
 
