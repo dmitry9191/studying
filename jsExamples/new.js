e5e5d321137650b1c5eec61559dcd06b6b1702cb
dmitry <dmipushkarjov@yandex.ru>
2021-04-02 22:18:34 +0300

    setget

--- jsExamples/new.js
+++ jsExamples/new.js
@@ -53,3 +53,48 @@ RegularExp classes:
 \S (not spaces)
 
 */
+
+class User {
+
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+    
+
+    #surname = 'dulin';
+
+    get sur() {
+        return this.#surname;
+    }
+
+    set sur(sur) {
+        this.#surname = sur;
+    }
+
+    say() {
+        console.log(`user name: ${this.name}, age: ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('invalid value');
+        }
+    }
+
+}
+
+
+const ivan = new User('ivan', 28);
+console.log(ivan.sur);
+
+ivan.sur = 'pipik';
+
+console.log(ivan.sur);
+
